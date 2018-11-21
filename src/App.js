e247d1eaabd347fcf37f95c7004bd3845641d456
anroniogi <anroniogi@icloud.com>
2018-11-22 03:50:42 +0900

    화면구성 끝

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './youtube-search.js'
+
+const API_KEY = 'AIzaSyCQZaqAqDtqyff4Syz5X285h3wVswCCh8c'
 
 class App extends Component {
+
+  
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <a>
+     <body className="body">
+       <div id="container" className="container">
+         <header className="header">
+           <h1 className="h1" >Search <span className="span" >Videos</span></h1>
+           <p>Search all YouTube Videos</p>
+         </header>
+         <section className="section">
+           <form id="search-form" className="search-form" method="get" action="">
+             <div class="fieldcontainer" className="fieldcontainer">
+               <input type="search" id="query" name="query" class="search-field" className="search-field" placeholder="Search YouTube"/>
+               <input type="submit" name="search-btn" class="search-btn" className="search-btn" value="검색" />
+             </div>
+           </form>
+
+           <ul id="results" class="item-list">
+             <li class="item" className="item">
+               <a className="a1" href="http://www.youtube.com/watch?v=e3Nl_TCQXuw" target="_blank">
+               <h3 className="h3">Beauty and the Beast – US Official Final Trailer</h3>
+               <div class="image-wrapper">
+                 <img className="img" src="https://i.ytimg.com/vi/e3Nl_TCQXuw/hqdefault.jpg"/>
+               </div>
+               <div class="description" className="description">
+                 <small>By <span class="channel-title" className="channel-title">Disney Movie Trailers</span> on <time>2017-01-31T02:44:39.000Z</time></small>
+                 <p className="p">The final trailer for Beauty and the Beast is here On March 17, rediscover a tale as old as time. Get your tickets now at BeOurGuest.com -- Disney's “Beauty ...</p>
+               </div>
+             </a></li>
+           </ul>
+         </section>
+       </div>
+     </body>
+     </a>
     );
   }
 }
